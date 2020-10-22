12713315479f8d5d06445accf8e380ab060156b8
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-22 11:45:21 +0200

    add for example london text to open api

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const inputMain = document.querySelector(".input");
 const mainButton = document.querySelector(".click");
 const pOfWarning = document.querySelector(".warning");
 const imgJpg = document.querySelector(".img");
-const infoWeather = document.querySelector(".weatherinfo");
-const temperature = document.querySelector(".temperature");
+const weather = document.querySelector(".weatherinfo");
+const temperature = document.querySelector(".temperatureText");
 
 
 const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const apiKeyCode = '&APPID=e4a2c8533c532e62cdf04aa291360d74';
+const apiKeyCode = '&appid=e4a2c8533c532e62cdf04aa291360d74';
 const units = '&units=metric';
 
 let city;
 let urlLink;
 
 const weatherSite = () => {
-    city = inputMain.value;
+    city = input.value;
     urlLink = apiLink + city + apiKeyCode + units;
 
     axios.get(urlLink)
@@ -24,8 +24,8 @@ const weatherSite = () => {
             const temper = res.data.main.temp;
             const actuallyStatus = Object.assign({}, ...res.data.weather);
 
-            temperature.textContent = Math.floor(temper) + '°C';
-            infoWeather.textContent = actuallyStatus.main;
+            temperatureText.textContent = Math.floor(temper) + '°C';
+            weather.textContent = actuallyStatus.main;
             nameOfCity.textContent = res.data.name;
         })
 };
