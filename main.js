067e2b64637f5f869a49d17c5737ec8089248fa6
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-22 10:58:07 +0200

    downloads elements on main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const nameOfCity = document.querySelector(".cityname");
+const inputMain = document.querySelector(".input");
+const mainButton = document.querySelector(".click");
+const pOfWarning = document.querySelector(".warning");
+const imgJpg = document.querySelector(".img");
+const infoWeather = document.querySelector(".weatherinfo");
+const temperature = document.querySelector(".temperature");
