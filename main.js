76c4baf808da8f87d16d7b91f66f330729bee25e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-23 10:19:09 +0200

    download unknown png to else / if status . id

--- main.js
+++ main.js
@@ -27,6 +27,25 @@ const weatherSite = () => {
             temperature.textContent = Math.floor(temper) + '°C';
             weather.textContent = actuallyStatus.main;
             nameOfCity.textContent = res.data.name;
+
+
+            if(status.id >= 200 && status.id < 300){
+                photo.setAttribute('src','../thunderstorm.png');
+            } else if (status.id >= 300 && status.id < 400) {
+                photo.setAttribute('src','../drizzle.png');
+            } else if (status.id >= 500 && status.id < 600){
+                photo.setAttribute('src','../rain.png');
+            } else if(status.id >= 600 && status.id < 700) {
+                photo.setAttribute('src','../ice.png');
+            } else if (status.id >= 700 && status.id < 800) {
+                photo.setAttribute('src','../fog.png');
+            }else if (status.id === 800){
+                photo.setAttribute('src','../sun.png');
+            } else if (status.id >= 800 && status.id < 900) {
+                photo.setAttribute('src','../cloud.png');
+            } else {
+                photo.setAttribute('src','../unknown.png');
+            }
         })
 };
 weatherSite();
